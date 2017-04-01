e277db7e67256883162ab651e5a0800346c899cf
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-01 16:24:00 +0100

    Update mainPage.php

--- mainPage.php
+++ mainPage.php
@@ -1,18 +1,17 @@
 <?php
-/*if(!isset($_SESSION['login'])) { //if login in session is not set
-    header("Location: LogInPage.php");
-}*/
+  session_start();
+  if(!isset($_SESSION['username'])) {header("Location: LogInPage.php");}
 ?>
 <!DOCTYPE HTML>
 <html>
 	<head>
 	    <div class="topnav" id="myTopnav">
 		 <?php
-		    /* if (isset($_SESSION["ul_id"]) && $_SESSION["ul_id"] != ''){/* NEED TO FIX SO LOGGED IN USER IS SET*/ 
+		      if (isset($_SESSION["username"]) && $_SESSION["username"] != ''){/* NEED TO FIX SO LOGGED IN USER IS SET*/ 
                  printf("<a href=\"./LogOut.php\"> Log Out</a>");
                  printf("<a href=\"./UserProfile.php\">Profile</a>");
                  printf("<a href=\"./FAQ.php\">FAQ</a>");
-				/*}	*/	
+				}	
             ?>   
         </div>
 		<title>Welcome to DocDoc</title>
@@ -55,28 +54,30 @@
 			                 $result = mysqli_query($conn,$sql);
 			                 if (mysqli_num_rows($result) > 0) {
 			                    while($row = mysqli_fetch_assoc($result)) {
+								$isFlagged=$row["flagged"];
+								if(!($isFlagged==1)){
 						          echo "<div class = \"boxed\">";
 				                  echo "<div class=\"h_iframe\">";
 				                  echo "<h1>" . $row["task_title"]. "</h1>";
 				                  echo "<p>Desciption: " . $row["task_description"].
 								  "<br>Pages: ".$row["task_pages"]."</p>";		
-							      echo "	 <div class=\"boxed2\">";
-						          $currentTask=$row["task_id"];
-				                  $tags="SELECT tag_name FROM `task_tag` where task_id = $currentTask";
+							      echo "<div class=\"boxed2\">";
+				                  $currentTask=$row["task_id"];
+				                  $tags="SELECT tag_name FROM `task_tag` where task_id = '$currentTask'";
 				                  $tresult=mysqli_query($conn,$tags);
-				                  $num=mysqli_num_rows($result);
-				                  $trow=mysqli_fetch_array($tresult,MYSQLI_ASSOC);
-				                  while($trow=mysqli_fetch_array($tresult, MYSQLI_ASSOC)){
-				                  echo "#" ,$trow["tag_name"]. ",";
-				                 }
+				                  $num=mysqli_num_rows($tresult);
+				                 while($trow=mysqli_fetch_array($tresult)){
+				                     echo "#" ,$trow["tag_name"]. ",";
+				                  }
 						         echo"</div>";
                                  echo"</div>";						  
                                  echo"</div>";						  
 						         echo"  <div class=\"boxed\">
-	                             <a class=\"link2\" href=\"./claimTask.php\">Claim Task </a>						 
-	                             <a class=\"link2\" href=\"./viewTask.php\">View Task</a>						 
-	                             <a class=\"link\" href=\"./flagTask.php\">Flag Task</a> 
+	                             <a class=\"link2\" href=\"./claimTaskCode.php?task_id=$currentTask\"\">Claim Task </a>						 
+	                             <a class=\"link2\" href=\"./viewTask.php?task_id=$currentTask\"\">View Task</a>						 
+	                             <a class=\"link\" href=\"./flagTask.php?task_id=$currentTask\">Flag Task</a> 
                                  </div><br><br>";
+								 }
 				                }
 			                  }       
                            $conn->close();
@@ -96,8 +97,8 @@
 				<!-- Nav -->
 					<nav id="nav">
 						<ul>
-						   <?php
-                             if (isset($_SESSION["ul_id"]) && $_SESSION["ul_id"] != '' && $_SESSION["is_Moderator"]=='1'){ 
+						   <?php			   
+                             if (isset($_SESSION["username"]) && $_SESSION["username"] != '' && $_SESSION["is_moderator"]==1){ 
                                 printf("<li class=\"current\"><a href=\"./mainPage.php\">Home</a></li>");
                                 printf("<li><a href=\"./myTasks.php\">My Tasks</a></li>");
                                 printf("<li><a href=\"./claimedTasks.php\">Claimed Tasks</a></li>");
