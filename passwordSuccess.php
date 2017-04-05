d8fb20a09d81ca31c23fec53a0a5b4a237587d4f
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-05 18:00:51 +0100

    Create passwordSuccess.php

---
+++ passwordSuccess.php
@@ -0,0 +1,101 @@
+<?php
+ session_start();
+?>
+<!DOCTYPE html>
+<html >
+<head>
+<link rel="stylesheet" href="assets/css/main.css" />
+  <link rel="style2" href="assets/css/bars/css"/>
+ <div class="topnav" id="myTopnav">
+      <a href="logInPage.php"> Log In</a>
+ </div> 
+ <?php
+ if (!isset ($_SESSION)) {
+			session_start();
+		}
+		session_unset();
+		session_destroy();	
+?>
+</head>
+<body>
+	<div id="content">
+	 <div class="inner">
+	 </div>
+	   <h1> Password Changed </h1>
+	   <p> You have successfully Changed your password. Please Log back in to continue.</p>
+	</div>
+	<div id="sidebar">
+
+				<!-- Logo -->
+					<h1 id="logo"><a href="logInPage.php"></a></h1>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul>
+							<li class="current"><a href="#">Logged Out</a></li>
+						</ul>
+					</nav>
+
+					<section class="box calendar">
+						<div class="inner">
+							<table>
+								<caption>March 2017</caption>
+								<thead>
+									<tr>
+										<th scope="col" title="Monday">M</th>
+										<th scope="col" title="Tuesday">T</th>
+										<th scope="col" title="Wednesday">W</th>
+										<th scope="col" title="Thursday">T</th>
+										<th scope="col" title="Friday">F</th>
+										<th scope="col" title="Saturday">S</th>
+										<th scope="col" title="Sunday">S</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td colspan="4" class="pad"><span>&nbsp;</span></td>
+										<td><span>1</span></td>
+										<td><span>2</span></td>
+										<td><span>3</span></td>
+									</tr>
+									<tr>
+										<td><span>4</span></td>
+										<td><span>5</span></td>
+										<td><span>6</span></td>
+										<td><span>7</span></td>
+										<td><span>8</span></td>
+										<td><span>9</span></td>
+										<td><a href="#">10</a></td>
+									</tr>
+									<tr>
+										<td><span>11</span></td>
+										<td><span>12</span></td>
+										<td><span>13</span></td>
+                                        <td><span>14</span></td>
+										<td><span>15</span></td>
+										<td><span>16</span></td>
+										<td><span>17</span></td>
+									</tr>
+									<tr>
+										<td><span>18</span></td>
+										<td><span>19</span></td>
+									  	<td class="today"><a href="#">20</a></td>
+										<td><span>21</span></td>
+										<td><span>22</span></td>
+										<td><span>23</span></td>
+										<td><span>24</span></td>
+									</tr>
+									<tr>
+										<td><span>25</span></td>
+										<td><span>26</span></td>
+										<td><span>27</span></td>
+										<td><span>28</span></td>
+										<td class="pad" colspan="3"><span>&nbsp;</span></td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</section>
+			</div>
+</body>
+</html>
