1d268f632a3c10633e64d27d108d0267e3ed33db
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-08 15:40:30 +0100

    Update flagTask.php

--- flagTask.php
+++ flagTask.php
@@ -10,8 +10,10 @@
       die("Connection failed: " . $conn->connect_error);
     }  
 	$currentT=$_GET['task_id'];
-	$sql ="INSERT INTO flagged_task(`task_id`, `pending _review`, `post_review_status`) VALUES (111,0,'');";
-	$sql .= "UPDATE task SET flagged='1' WHERE task_id='$currentT'"; //Match ID of task stream not established yet
+    $userID =$_GET['userID'];
+	$sql ="INSERT INTO flagged_task(`task_id`, `pending _review`, `post_review_status`) VALUES ($currentT,0,'');";
+	$sql .= "UPDATE task SET flagged='1' WHERE task_id='$currentT';";
+	$sql .="UPDATE user SET `reputation`= (reputation + 2) WHERE ul_id='$userID'";
     mysqli_multi_query($conn, $sql) or die("MySQL Error: " . mysqli_error($conn) . "<hr>\nQuery: $sql");
     $conn->close();
 	header("Location: mainPage.php");
