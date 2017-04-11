269e5301c44f3717450f7972d2719d552c49b2ba
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-11 14:21:55 +0100

    Create bannedPage.php

---
+++ bannedPage.php
@@ -0,0 +1,85 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html >
+<head>
+<title>Banned</title>
+<link rel="stylesheet" href="assets/css/main.css"/>
+<div class="topnav" id="myTopnav">
+		    <a href="#" onclick="history.go(-1);">Back</a>  
+</div>
+</head>
+<body>
+<div id="content">
+	<div class="inner">
+    <h2> Sorry! Your account is banned</h2>
+	<p> Due to a violation of our websites terms, your account has been banned from accessing our website</p>
+    </div>
+</div>
+<div id="sidebar">
+	<h1 id="logo"><a href="logInPage.php"></a></h1>
+	<section class="box calendar">
+						<div class="inner">
+							<table>
+								<caption>March 2017</caption>
+								<thead>
+									<tr>
+										<th scope="col" title="Monday">M</th>
+										<th scope="col" title="Tuesday">T</th>
+										<th scope="col" title="Wednesday">W</th>
+										<th scope="col" title="Thursday">T</th>
+										<th scope="col" title="Friday">F</th>
+										<th scope="col" title="Saturday">S</th>
+										<th scope="col" title="Sunday">S</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td colspan="4" class="pad"><span>&nbsp;</span></td>
+										<td><span>1</span></td>
+										<td><span>2</span></td>
+										<td><span>3</span></td>
+									</tr>
+									<tr>
+										<td><span>4</span></td>
+										<td><span>5</span></td>
+										<td><span>6</span></td>
+										<td><span>7</span></td>
+										<td><span>8</span></td>
+										<td><span>9</span></td>
+										<td><a href="#">10</a></td>
+									</tr>
+									<tr>
+										<td><span>11</span></td>
+										<td><span>12</span></td>
+										<td><span>13</span></td>
+                                        <td><span>14</span></td>
+										<td><span>15</span></td>
+										<td><span>16</span></td>
+										<td><span>17</span></td>
+									</tr>
+									<tr>
+										<td><span>18</span></td>
+										<td><span>19</span></td>
+									  	<td class="today"><a href="#">20</a></td>
+										<td><span>21</span></td>
+										<td><span>22</span></td>
+										<td><span>23</span></td>
+										<td><span>24</span></td>
+									</tr>
+									<tr>
+										<td><span>25</span></td>
+										<td><span>26</span></td>
+										<td><span>27</span></td>
+										<td><span>28</span></td>
+										<td class="pad" colspan="3"><span>&nbsp;</span></td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</section>
+</div>
+  			<script src="assets/js/sampJs.js"></script>
+</body>
+</html>
