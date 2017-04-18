0557528321bb4f59e91f40c1378cfd9798b0a118
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-18 10:09:37 +0100

    Update insertUser.php

--- insertUser.php
+++ insertUser.php
@@ -13,13 +13,15 @@
 		$firstName = (isset($_POST['firstName'])?$_POST['firstName']:'');
 		$lastName = (isset($_POST['lastName'])?$_POST['lastName']:'');
 		$field = (isset($_POST['field'])?$_POST['field']:'');
+		$password = (isset($_POST['passw'])?$_POST['passw']:'');
 		
-		$sql="INSERT INTO `user`(`ul_id`, `ul_email`, `first_name`, `last_name`, `field`, `reputation`, `is_moderator`, `is_banned`, `has_deleted`) VALUES ('$IDnum', '$email', '$firstName', '$lastName', '$field', '0', '0', '0', '0')";
-		mysqli_query($conn,$sql);
+		$sql="INSERT INTO `user`(`ul_id`, `ul_email`, `first_name`, `last_name`, `field`, `reputation`, `is_moderator`, `is_banned`, `has_deleted`) VALUES ('$IDnum', '$email', '$firstName', '$lastName', '$field', '0', '0', '0', '0');";
+		$sql.="INSERT INTO `log_in`(`ul_id`, `password`) VALUES ('$IDnum','$password')";
+		mysqli_multi_query($conn, $sql) or die("MySQL Error: " . mysqli_error($conn) . "<hr>\nQuery: $sql");
 		if(mysqli_affected_rows($conn)>0){
-			echo "Success";
+			  header("Location: LogInPage.php");
 		}
-		else{echo "Failure";}
+		else{  header("Location: RegistrationForm.php");}
 		$conn->close();
 
 ?>
