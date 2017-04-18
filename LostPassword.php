25112c0834d0b0ffed3a63d5f3b86904d2194c01
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-18 10:21:51 +0100

    Update LostPassword.php

--- LostPassword.php
+++ LostPassword.php
@@ -2,23 +2,18 @@
 <html >
 <link rel="stylesheet" href="assets/css/login.css">
 <link rel="stylesheet" href="assets/css/bars.css">
-<head>
-<div class="topnav" id="myTopnav">
-           <a href="FAQ.php">FAQ</a>           
-</div>
-</head>
+<title>Password Recovery</title>
 <body>
 <div class="container">
 	<section id="content">
-         <form action="#"><!--Need to get functionality implemented with this-->
+         <form action="emailSuccess.php">
              <h1>Recovery</h1>
 			 <p> Please enter your registered email to recover your password.</p>
 			 <p> An email will be sent to your email address to recover your password</p>
-             <input type="email" name="email" pattern="[0-9]{6,7,8,9}@(studentmail.ul.ie|ul.ie)" placeholder="Enter your email" required>
+             <input type="email" name="email" pattern="[0-9.-_]{6,9}@(studentmail.ul.ie|ul.ie)" placeholder="Enter your email" required>
 			 <div>
              <input type="submit" value="Send Email">
 			 <a href="LogInPage.php">Cancel</a>
-			 <a href="LostPassword.php">Re-Send Email</a>
 			 </div>
         </form>
     </section>
