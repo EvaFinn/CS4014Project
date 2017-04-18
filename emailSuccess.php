44801cfe6551738196bbe0d2c584e5e021615b72
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-18 10:22:11 +0100

    Create emailSuccess.php

---
+++ emailSuccess.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html >
+<link rel="stylesheet" href="assets/css/login.css">
+<link rel="stylesheet" href="assets/css/bars.css">
+<title>Password Recovery</title>
+<body>
+<div class="container">
+	<section id="content">
+         <form action="LostPassword.php">
+             <h1>Email Sent</h1>
+			 <p> Please check your registered email address for a password recovery email</p>     	
+             <div>			 
+			  <input type="submit" value="Re-Send Email">
+			  <a href="LogInPage.php">Back</a>
+			   </div>
+             </form>
+    </section>
+</div>
+</body>
+</html>
