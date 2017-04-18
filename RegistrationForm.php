518eea6cb765d1a983f53723f69c3ad50ec80444
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-18 10:43:48 +0100

    Update RegistrationForm.php

--- RegistrationForm.php
+++ RegistrationForm.php
@@ -6,26 +6,26 @@
   <body>  
 	<div class="container">
 	  <section id="content">
-       <form action="insertUser.php" method="POST">
+     <form action="insertUser.php" method="POST">
 	     <h1> Registation Form </h1>
 		    <div>
-                 <input type="text" name="email" pattern="[0-9.-_]{6,9}@(studentmail.ul.ie|ul.ie)" placeholder="Enter your email" required>
+                 <input type="text" name="email" pattern="[0-9.-_]{6,9}@(studentmail.ul.ie|ul.ie)" placeholder="Enter your email" required oninvalid="setCustomValidity('Please enter a valid UL email')"/>
 		    </div>
 		    <div>
-                 <input type="text" placeholder="UL ID Number" pattern="[0-9]{6,9}" name="IDnum" required>
+                 <input type="text" placeholder="UL ID Number" pattern="[0-9]{6,9}" name="IDnum" required oninvalid="setCustomValidity('Please enter a valid UL ID')"/>
             </div>
 			<div>
-	             <input type="text" placeholder="name" name="firstName" required>
+	             <input type="text" placeholder="name" name="firstName" required/>
 			</div>
             <div>
-	             <input type="text" placeholder="surname" name="lastName" required> 
+	             <input type="text" placeholder="surname" name="lastName" required/>
 		    </div>
 			<div>
-			     <input type="text" placeholder="Field Of Study" name="field" required>
+			     <input type="text" placeholder="Field Of Study" name="field" required/>
 		    </div>
-	             <div> 
-			    <input type="password" placeholder="Password" name="passw" required>
-			</div>	
+			<div> 
+			    <input type="password"  pattern="[a-zA-Z0-9]{6,}" placeholder="Password" name="passw" required  oninvalid="setCustomValidity('Password must be at least 6 characters long')"/>
+			</div>			   
 			<div>
 	             <input type="submit" value="Register" name="register">
 				 <a href="LogInPage.php">Cancel</a>
