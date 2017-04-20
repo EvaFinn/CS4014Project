8b28d99a1c607acc8cc5407ae21483b458c4fede
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-20 13:33:27 +0100

    Update cancelReview.php

--- cancelReview.php
+++ cancelReview.php
@@ -32,6 +32,28 @@
 </div>
 <div id="sidebar">
 	<h1 id="logo"><a href="mainPage.php"></a></h1>
+	<!-- Nav -->
+					<nav id="nav">
+						<ul>
+						 <?php			   
+                             if (isset($_SESSION["username"]) && $_SESSION["username"] != '' && $_SESSION["is_moderator"]==1){ 
+                                printf("<li><a href=\"./mainPage.php\">Home</a></li>");
+                                printf("<li><a href=\"./myTasks.php\">My Tasks</a></li>");
+                                printf("<li><a href=\"./mytags.php\">Favourited Tags</a></li>");
+                                printf("<li><a href=\"./claimedTasks.php\">Claimed Tasks</a></li>");
+								printf("<li><a href=\"./ModTasks.php\">Moderator Tasks</a></li>");
+								
+				            }
+                            else{
+							    printf("<li><a href=\"./mainPage.php\">Home</a></li>");
+                                printf("<li><a href=\"./myTasks.php\">My Tasks</a></li>");
+                                printf("<li><a href=\"./myTags.php\">Favourited Tags</a></li>");
+                                printf("<li><a href=\"./claimedTasks.php\">Claimed Tasks</a></li>");
+								
+							}							
+                           ?>   
+						</ul>
+					</nav>
 	<section class="box calendar">
 						<div class="inner">
 							<table>
