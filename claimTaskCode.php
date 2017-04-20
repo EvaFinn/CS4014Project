335d3458c0962ff6626e22f93f352dc2900bd13c
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-20 14:02:44 +0100

    Update claimTaskCode.php

--- claimTaskCode.php
+++ claimTaskCode.php
@@ -1,25 +1,20 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "softwarepro";
+$username = "";
+$password = "";
 $db_name = "docdoc"; 
-$tbl_name = "deleted_tasks";
+$userID  = $_GET['userID'];
+$currentT=$_GET['task_id']; 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $db_name);
+ $conn = new mysqli($servername, $username, $password, $db_name);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-$sql = "INSERT INTO claimed (task_id)
-VALUES ()";
-$sql2="UPDATE claimed FROM task WHERE task_id=''";
-
-if ($conn->query($sql) === TRUE) {
-    $success_message = "New record created successfully";
-	header("Location: ModTasks.php");
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
+$sql = "INSERT INTO `claimed_tasks`(`task_id`, `hidden_id`) VALUES ($currentT,$userID);";
+$sql .= "UPDATE `task` SET `claimed` = '1' WHERE `task`.`task_id` = $currentT;";
+$sql .="UPDATE `user` SET `reputation`= (reputation + 10) WHERE ul_id='$userID'";
+mysqli_multi_query($conn, $sql) or die("MySQL Error: " . mysqli_error($conn) . "<hr>\nQuery: $sql");
+header("Location: mainPage.php");
 $conn->close();
 ?>
