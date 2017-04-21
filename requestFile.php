2c933fa30c0e3bcad80ffe605c3ed02abc3ab1f6
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-21 14:39:28 +0100

    Update requestFile.php

--- requestFile.php
+++ requestFile.php
@@ -29,7 +29,7 @@
 	$host = "localhost"; 
     $username = "";
     $password = ""; 
-    $db_name = "docdoc"; 
+    $db_name = ""; 
     $conn = new mysqli($host, $username, $password, $db_name);
    if ($conn->connect_error) {
                              die("Connection failed: " . $conn->connect_error);
