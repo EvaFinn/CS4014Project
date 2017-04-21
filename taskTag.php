adde698bcec4d25cac3c145a184e34d850b31778
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-21 14:36:27 +0100

    Update taskTag.php

--- taskTag.php
+++ taskTag.php
@@ -8,7 +8,7 @@
             $host = "localhost"; //host name
             $username = ""; //username
             $password = ""; //pass
-            $db_name = "docdoc"; 
+            $db_name = ""; 
 		    $conn = new mysqli($host, $username, $password, $db_name);
             if ($conn->connect_error) {
                              die("Connection failed: " . $conn->connect_error);
