d77c14da687a84c5448b384d34c55d97a2d8efd0
aoifepower123 <15140679@studentmail.ul.ie>
2017-04-21 14:51:38 +0100

    Update E-Mail.php

--- E-Mail.php
+++ E-Mail.php
@@ -1,30 +1,12 @@
 <?php
 
-// Table Scheme for Verify Table
-CREATE TABLE `verify` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `email` text NOT NULL,
- `password` text NOT NULL,
- `code` text NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1
-
-// Table Scheme for verified_user table
-CREATE TABLE `verified_user` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `email` text NOT NULL,
- `password` text NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1
-
-
 if(isset($_POST['register']))
 {
 	$email_id=$_POST['email'];
 	$pass=$_POST['password'];
 	$code=substr(md5(mt_rand()),0,15);
-	mysql_connect('localhost','root','');
-	mysql_select_db('DocDoc');
+	mysql_connect('','','');
+	mysql_select_db('');
 	
 	$insert=mysql_query("insert into verify values('','$email','$pass','$code')");
 	$db_id=mysql_insert_id();
